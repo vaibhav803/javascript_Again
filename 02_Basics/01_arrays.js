310b9480be6b8dd66dd8ecafb6071b01d6dcfda9
gitcoder <yo@mail.com>
2026-02-21 00:14:29 +0530

    array discovered

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,31 @@
+// array
+
+
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ["shaktiman","wifiman"]
+
+const Arr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+//push pop unshift
+
+//shift
+
+//indexOf
+//includes
+
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+const myn1 = myArr.slice(1,4)
+console.log(myArr);
+
+const myn2 = myArr.splice(1,4)
+console.log(myArr);
+
+//slice splice find findsome
+
