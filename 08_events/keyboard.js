92a8079e9bab2427c4f4f064926eefc30fdf582b
gitcoder <yo@mail.com>
2026-02-22 16:57:26 +0530

    event handling with inifinite colors and i/o detection

---
+++ 08_events/keyboard.js
@@ -0,0 +1,16 @@
+const insert = document.getElementById('insert')
+
+
+window.addEventListener('keydown',(e) => {
+    insert.innerHTML = `
+    <div class='color'>
+    
+    </div>
+    `
+
+
+    //e.keyCode
+    //e.key
+    //e.code
+    //e.key === " "? "space" : e.key
+})
